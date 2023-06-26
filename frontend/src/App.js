67e400111493f9e7cfeed3a0c54ba57ebf6288fc
this-is-singh19 <thisissingh19@gmail.com>
2023-06-26 20:44:39 +0530

    Developed structure of homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Container } from 'react-bootstrap' 
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
-      My app
+      <Header />
+      <main>
+      <Container className='py-3'>
+        <h1>Welcome to indishop</h1>
+      </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
