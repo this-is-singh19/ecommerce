dba098e93663affad1806fd2947fdcb6b7425a74
this-is-singh19 <thisissingh19@gmail.com>
2023-06-28 17:22:56 +0530

    backend added and integrated with frontend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,22 @@
-import React from 'react'
+/*
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
-  const { id } = useParams();
-  const product = products.find((p) => String(p._id) === id);
+  const [product, setProduct] = useState([])
 
-  if (!product) return null; // or fallback UI
+  useEffect(()=> {
+      async function fetchProduct(){
+        const { data } = await axios.get(`/api/products/${match.params.id}`)
+        setProduct(data)
+      }
 
+      fetchProduct()
+
+  } , [])
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>Back</Link>
@@ -66,3 +73,81 @@ function ProductScreen() {
   );
 }
 export default ProductScreen
+*/
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import axios from 'axios';
+
+function ProductScreen() {
+  const { id } = useParams(); // Extract the "id" parameter from the URL
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, [id]);
+
+  return (
+    <div>
+      <Link to='/' className='btn btn-light my-3'>
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </ListGroup.Item>
+
+            <ListGroup.Item>Price: Rs. {product.price}</ListGroup.Item>
+
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>Rs. {product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{product.countInStock > 0 ? 'Available' : 'Out of stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button className='btn btn-danger' type='button' disabled={product.countInStock === 0}>
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
