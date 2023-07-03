b173ce8903939f2e1cc701d5d7a18016a7ce12bb
this-is-singh19 <thisissingh19@gmail.com>
2023-07-03 20:51:27 +0530

    up

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -45,7 +45,7 @@ function CartScreen() {
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
                   <Col md={2}>
-                    ${item.price}
+                    Rs.{item.price}
                   </Col>
                   <Col md={2}>
                       <Form.Control 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -23,7 +23,7 @@ function HomeScreen() {
             :
             <Row>
               {products.map(product => (
-                <Col key={product._id} sm={12} md={6} Lg={4} xL={3}>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                   <Product product={product} />
                 </Col>
               )
