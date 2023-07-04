fbbee3e15cc8e9be1bae1f8706a1f6fe19d40e0f
this-is-singh19 <thisissingh19@gmail.com>
2023-07-04 17:24:04 +0530

    User register functionality initiated at frontend

--- backend/base/views.py
+++ backend/base/views.py
@@ -9,6 +9,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(self.user)
@@ -23,19 +25,22 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 # Create your views here.
-@api_view(['GET'])
-def getRoutes(request):
-    routes=[
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload',
-        '/api/products/<id>/reviews',
-        '/api/products/top/',
-        '/api/products/<id>',
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>',
-    ]
-    return Response(routes)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer=UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email exists'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
