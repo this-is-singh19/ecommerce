c30115540b3e369032a267863f98e9cf9909b0ba
this-is-singh19 <thisissingh19@gmail.com>
2023-07-08 20:21:38 +0530

    User update functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 function App() {
   return (
     <Router>
@@ -40,6 +41,7 @@ function App() {
             <Route path='/' element={<HomeScreen />} />
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/product/:id' element={< ProductScreen />} />
             <Route path='/cart/:id?' element={< CartScreen />} />
           </Routes>
